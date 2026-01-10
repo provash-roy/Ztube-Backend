7ba8bf388886e5a6ab9bf2bb5e848b470eb28455
Provash-roy <provashray94@gmail.com>
2026-01-10 23:41:48 +0600

    Implement database connection and configuration

---
+++ src/constants/index.js
@@ -0,0 +1,5 @@
+const DB_NAME = "Ztube";
+const DB_USER = "Provash";
+const DB_PASSWORD = "provashrayprovash";
+
+export { DB_NAME, DB_USER, DB_PASSWORD };
--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 const express = require("express");
+const { connect } = require("mongoose");
 const app = express();
 require("dotenv").config();
 const PORT = process.env.PORT || 3000;
+
+const connectDB = require("./db");
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+connectDB()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.error("Failed to connect to the database:", error);
+  });
