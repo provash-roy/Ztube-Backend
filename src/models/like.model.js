680612699f70ff3ae46a7397ed41b58750341451
Provash-roy <provashray94@gmail.com>
2026-02-05 23:44:07 +0600

    Add Like and Tweet models

---
+++ src/models/like.model.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+const likeSchema = new mongoose.Schema(
+  {
+    video: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Video",
+    },
+    tweet: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Tweet",
+    },
+    comment: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Comment",
+    },
+    likedBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  { timestamps: true },
+);
+
+const Like = mongoose.model("Like", likeSchema);
+export default Like;
