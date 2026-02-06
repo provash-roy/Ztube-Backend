138f424fae49c6d276d55ca3342d93fd42dd36ee
Provash-roy <provashray94@gmail.com>
2026-02-06 22:52:21 +0600

    Add video controller and JWT authentication middleware

---
+++ src/controllers/video.controller.js
@@ -0,0 +1,6 @@
+import User from "../models/user.model";
+import Video from "../models/video.model";
+import ApiError from "../utils/apiError";
+import ApiResponse from "../utils/apiResponse";
+import asyncHandler from "../utils/asyncHandler";
+
