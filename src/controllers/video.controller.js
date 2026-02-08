730b9652accf8132c60bdfe490875f0116084243
Provash-roy <provashray94@gmail.com>
2026-02-08 23:25:23 +0600

    Add video fetching and uploading functionality

--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -1,6 +1,68 @@
-import User from "../models/user.model";
-import Video from "../models/video.model";
-import ApiError from "../utils/apiError";
 import ApiResponse from "../utils/apiResponse";
 import asyncHandler from "../utils/asyncHandler";
 
+const getAllVideos = asyncHandler(async (req, res) => {
+  const search = req.query.search || "";
+  const sortQuery = req.query.sort || "createdAt_desc";
+
+  const page = parseInt(req.query.page) || 1;
+  const limit = parseInt(req.query.limit) || 5;
+
+  const query = search ? { title: { $regex: search, $options: "i" } } : {};
+
+  const [field, order] = sortQuery.split("_");
+  const sort = { [field]: order === "asc" ? 1 : -1 };
+
+  const aggregatePipeline = [
+    { $match: query },
+    { $sort: sort },
+    {
+      $lookup: {
+        from: "users",
+        localField: "owner",
+        foreignField: "_id",
+        as: "channel",
+      },
+    },
+    { $unwind: "$channel" },
+    {
+      $project: {
+        _id: 1,
+        thumbnail: 1,
+        title: 1,
+        duration: 1,
+        views: {
+          $cond: {
+            if: { $isArray: "$views" },
+            then: { $size: "$views" },
+            else: { $ifNull: ["$views", 0] },
+          },
+        },
+        isPublished: 1,
+        "channel._id": 1,
+        "channel.username": 1,
+        "channel.avatar": 1,
+        createdAt: 1,
+        updatedAt: 1,
+      },
+    },
+  ];
+
+  const aggregate = Video.aggregate(aggregatePipeline);
+  const result = await Video.aggregatePaginate(aggregate, { page, limit });
+
+  if (!result) {
+    return new ApiError(400, "Failed to fetch videos");
+  }
+
+  return ApiResponse(res, 200, "Videos fetched successfully", result);
+});
+
+const uploadAVideo = asyncHandler(async (req, res) => {
+  const { title, description } = req.body;
+  if (!(title || description)) {
+    new ApiResponse(400, "Title or Description is invalid");
+  }
+});
+
+export { getAllVideos };
